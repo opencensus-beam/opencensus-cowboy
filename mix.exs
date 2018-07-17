45cd2272ac23fe50441d85c7df0f48ac83f2e331
Ilya Khaprov <i.khaprov@gmail.com>
2018-07-17 18:01:00 +0300

    Bump to v0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OpencensusCowboy.Mixfile do
 
   def project do
     [app: :opencensus_cowboy,
-     version: "0.1.1",
+     version: "0.2.0",
      deps: deps(),
      description: description(),
      package: package()]
