93b35bdddbebf1b22921d3439ee214402d331d9e
Łukasz Niemier <lukasz@niemier.pl>
2019-03-21 17:25:30 +0100

    Cleanup lint errors

--- src/opencensus_cowboy2_instrumenter.erl
+++ src/opencensus_cowboy2_instrumenter.erl
@@ -8,7 +8,6 @@
 %% <ul>
 %%   <li>
 %%     `cowboy_early_errors_total'<br/>
-%%     Type: counter.<br/>
 %%     Tags: default - `[]', configured via `early_errors_tags'.<br/>
 %%     Total number of Cowboy early errors, i.e. errors that occur before a request is received.
 %%   </li>
@@ -21,58 +20,30 @@
 %%   </li>
 %%   <li>
 %%     `cowboy_requests_total'<br/>
-%%     Type: counter.<br/>
 %%     Tags: default - `[method, reason, status_class]', configured via `request_tags'.<br/>
 %%     Total number of Cowboy requests.
 %%   </li>
 %%   <li>
 %%     `cowboy_spawned_processes_total'<br/>
-%%     Type: counter.<br/>
 %%     Tags: default - `[method, reason, status_class]', configured via `request_tags'.<br/>
 %%     Total number of spawned processes.
 %%   </li>
 %%   <li>
 %%     `cowboy_errors_total'<br/>
-%%     Type: counter.<br/>
 %%     Tags: default - `[method, reason, error]', configured via `error_tags'.<br/>
 %%     Total number of Cowboy request errors.
 %%   </li>
 %%   <li>
 %%     `cowboy_request_duration_seconds'<br/>
-%%     Type: histogram.<br/>
 %%     Tags: default - `[method, reason, status_class]', configured via `request_tags'.<br/>
-%%     Buckets: default - `[0.01, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 4]',
-%%     configured via `duration_buckets'.<br/>
 %%     Cowboy request duration.
 %%   </li>
 %%   <li>
 %%     `cowboy_receive_body_duration_seconds'<br/>
-%%     Type: histogram.<br/>
 %%     Tags: default - `[method, reason, status_class]', configured via `request_tags'.<br/>
-%%     Buckets: default - `[0.01, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 4]',
-%%     configured via `duration_buckets'.<br/>
 %%     Request body receiving duration.
 %%   </li>
 %% </ul>
-%%
-%% ==Configuration==
-%%
-%% Prometheus Cowboy2 instrumenter configured via `cowboy_instrumenter' key of `prometheus'
-%% app environment.
-%%
-%% Default configuration:
-%%
-%% <pre lang="erlang">
-%% {prometheus, [
-%%   ...
-%%   {cowboy_instrumenter, [{duration_buckets, [0.01, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 4]},
-%%                          {early_error_tags,  []},
-%%                          {request_tags, [method, reason, status_class]},
-%%                          {error_tags, [method, reason, error]},
-%%                          {registry, default}]}
-%%   ...
-%% ]}
-%% </pre>
 %%
 %% ==Tags==
 %%
@@ -86,7 +57,7 @@
 %%  - error.
 %%
 %% ===Custom tags===
-%% can be implemented via module exporting tag_value/2 function.
+%% can be implemented via module exporting `tag_value/2' function.
 %% First argument will be tag name, second is Metrics data from
 %% <a href="https://github.com/ninenines/cowboy/blob/master/src/cowboy_metrics_h.erl">
 %% metrics stream handler
@@ -119,11 +90,11 @@ observe(Metrics=#{ref:=ListenerRef}) ->
 %% Sets all metrics up. Call this when the app starts.
 %% @end
 setup() ->
-  oc_stat_measure:new(cowboy_early_errors, "Counts Cowboy early errors.", 1),
-  oc_stat_measure:new(cowboy_protocol_upgrades, "Counts protocol upgrades.", 1),
+  oc_stat_measure:new(cowboy_early_errors, "Counts Cowboy early errors.", req),
+  oc_stat_measure:new(cowboy_protocol_upgrades, "Counts protocol upgrades.", req),
   oc_stat_measure:new(cowboy_request_duration, "Cowboy request duration.", native_time_unit),
-  oc_stat_measure:new(cowboy_spawned_processes, "Counts spawned processes.", 1),
-  oc_stat_measure:new(cowboy_errors, "Counts request errors.", 1),
+  oc_stat_measure:new(cowboy_spawned_processes, "Counts spawned processes.", req),
+  oc_stat_measure:new(cowboy_errors, "Counts request errors.", req),
   oc_stat_measure:new(cowboy_receive_body_duration, "Time needed to receive full body.",
                       native_time_unit),
 
